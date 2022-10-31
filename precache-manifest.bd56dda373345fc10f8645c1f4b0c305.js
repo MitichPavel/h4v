fffd3724d1f141fc1c2e4e7892642503e4f512ef
Pavlo Mitich <mitich.pavel@gmail.com>
2022-10-31 18:02:43 +0100

    Updates

---
+++ precache-manifest.bd56dda373345fc10f8645c1f4b0c305.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c047a5bc4f0aacdd1aeb",
+    "url": "/h4v/css/app.c97a25c9.css"
+  },
+  {
+    "revision": "21ef0f7f7d83e9472df9",
+    "url": "/h4v/css/chunk-12875e78.537e515e.css"
+  },
+  {
+    "revision": "89f29efddc250d691cf7",
+    "url": "/h4v/css/chunk-13fd828c.6cc42a69.css"
+  },
+  {
+    "revision": "719e47d7d44898f21044",
+    "url": "/h4v/css/chunk-28387c86.4ef9cbc8.css"
+  },
+  {
+    "revision": "ed9c85e1aea2ab97ff59ef59ad52ca67",
+    "url": "/h4v/img/loader_dots.ed9c85e1.svg"
+  },
+  {
+    "revision": "db66649c7192f07f92d9b3698d55d1ab",
+    "url": "/h4v/img/loader_white.db66649c.svg"
+  },
+  {
+    "revision": "255c0d477027207ec36133c45ef599f3",
+    "url": "/h4v/img/user.255c0d47.svg"
+  },
+  {
+    "revision": "77116870605ca68963cd2c5b695a6125",
+    "url": "/h4v/img/x.77116870.svg"
+  },
+  {
+    "revision": "2ecb60cad69c33359b0a12a16ad31761",
+    "url": "/h4v/img/zoom_in.2ecb60ca.svg"
+  },
+  {
+    "revision": "da2543af49d69cd404e0ffa8a947ae0d",
+    "url": "/h4v/img/zoom_out.da2543af.svg"
+  },
+  {
+    "revision": "5c9a27e740f1c9daf143bb11c116728e",
+    "url": "/h4v/index.html"
+  },
+  {
+    "revision": "c047a5bc4f0aacdd1aeb",
+    "url": "/h4v/js/app.79b284ed.js"
+  },
+  {
+    "revision": "21ef0f7f7d83e9472df9",
+    "url": "/h4v/js/chunk-12875e78.c7452e88.js"
+  },
+  {
+    "revision": "89f29efddc250d691cf7",
+    "url": "/h4v/js/chunk-13fd828c.6a3cfb07.js"
+  },
+  {
+    "revision": "719e47d7d44898f21044",
+    "url": "/h4v/js/chunk-28387c86.39904b5c.js"
+  },
+  {
+    "revision": "cc1ce84b395a3f797e2d",
+    "url": "/h4v/js/chunk-vendors.e8879a51.js"
+  },
+  {
+    "revision": "2bbebf55cc54a7bd566d57b3272a00a0",
+    "url": "/h4v/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/h4v/robots.txt"
+  }
+]);
